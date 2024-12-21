606e1b123f3ca2232c8852c9147a00b1000f704f
Morgensternli <moba.morgenstern@gmail.com>
2024-12-21 17:49:57 +0100

    DNeues Projekt

--- digital-guard/src/routes/+page.svelte
+++ digital-guard/src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to ScreenStack</h1>
-<p>Your tracker and search engine for movies and tv shows</p>
+<h1>Wilkommen zu Digital Guard</h1>
+<p>In einer Welt, in der unser digitales Leben immer mehr an Bedeutung gewinnt, stellen wir die Frage: 
+    Was passiert mit Ihren Daten, Konten und digitalen Besitztümern, wenn Sie einmal nicht mehr da sind? 
+    Mit Digital Guard bieten wir Ihnen eine sichere und einfache Lösung, um Ihren digitalen Nachlass zu organisieren und Ihre Wünsche für den Umgang damit festzulegen.
+
+    Verwalten Sie all Ihre digitalen Konten an einem Ort.
+    Hinterlassen Sie klare Anweisungen für den Todesfall.
+    Bestimmen Sie vertrauenswürdige Personen, die Zugriff erhalten.
+    Schützen Sie Ihre Daten mit höchsten Sicherheitsstandards.
+    Übernehmen Sie die Kontrolle über Ihre digitale Zukunft - mit Digital Guard. Sicher. Einfach. Für Sie.</p>
